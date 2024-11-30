b4b37667a6cb733a22054c5e19631e7ca1695886
kumar harsh <harsh@kumars-MacBook-Air.local>
2024-11-30 18:02:21 +0530

    clean code

--- components/ui/logo.tsx
+++ components/ui/logo.tsx
@@ -4,10 +4,10 @@ import logoSvg from "@/public/images/logo-svg.svg";
 import { useRouter } from 'next/router';
 
 export default function Logo() {
-  const { basePath } = useRouter();
-
+  // const { basePath } = useRouter();
+  const basePath = process.env.BASE_PATH 
   return (
-    <Link href="/" passHref>
+    <Link href="/" legacyBehavior>
       <a className="block" aria-label="FactFortress">
         <Image src={`${basePath}/images/logo-svg.svg`} alt="Fact Fortress Logo" width={60} height={40} />
       </a>
