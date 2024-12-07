8869927312d7040ebb16cdffbf5f84d7de992855
belugawhalesam <sameermohd2k1@gmail.com>
2024-12-08 00:56:16 +0530

    logo-demo-rmDocs

--- components/hero-checker.tsx
+++ components/hero-checker.tsx
@@ -2,7 +2,8 @@
 
 import React from "react";
 import YouTube from "react-youtube";
-import Link from "next/link";
+import Image from "next/image";
+import Logo from "@/public/images/logo.jpeg";
 import { Cover } from "@/components/ui/cover";
 import { BackgroundGradientAnimation } from "@/components/ui/background-gradient-animation";
 import { FlipWords } from "@/components/ui/flip-words";
@@ -26,11 +27,20 @@ export default function Hero() {
           <div className="pt-32 pb-12 md:pt-40 md:pb-20">
             {/* Section header */}
             <div className="text-center pb-12 md:pb-16">
+              <div className="flex justify-center items-center my-4">
+                <Image
+                  src={Logo}
+                  alt="Overview"
+                  width={300} // Adjust size
+                  height={300}
+                  className="rounded-full border-2 border-gray-300"
+                />
+              </div>{" "}
               <h1 className="text-2xl md:text-2xl lg:text-4xl font-semibold max-w-7xl mx-auto text-center mt-6ls relative z-20 py-6 bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-neutral-700 to-neutral-700 dark:from-neutral-800 dark:via-white dark:to-white">
-                On-Chain <br />
                 <Cover>Zero-Knowledge Proof Framework</Cover>
                 <br />
-                for <FlipWords words={words} />Fact-Checking
+                for <FlipWords words={words} />
+                Fact-Checking
               </h1>
             </div>
             {/* Hero image */}
